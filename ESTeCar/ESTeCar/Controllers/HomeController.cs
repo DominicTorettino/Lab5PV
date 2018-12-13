3ea6666d9edaffb2924b2419aeaff1eb4b7b6870
TTTavares <43744305+TTTavares@users.noreply.github.com>
2018-12-13 16:52:37 +0000

    Lab 5 PV

---
+++ ESTeCar/ESTeCar/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ESTeCar.Models;
+
+namespace ESTeCar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("Acerca")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "";
+
+            return View("About");
+        }
+
+        [ActionName("Contactos")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "";
+
+            return View("Contact");
+        }
+
+        [ActionName("Privacidade")]
+        public IActionResult Privacy()
+        {
+            return View("Privacy");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ESTeCar/ESTeCar/Migrations/20181111193248_restrictions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ESTeCar.Migrations
+{
+    public partial class restrictions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumeroDePassageiros",
+                table: "Carro");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Designacao",
+                table: "Marca",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Modelo",
+                table: "Carro",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Designacao",
+                table: "Marca",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Modelo",
+                table: "Carro",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumeroDePassageiros",
+                table: "Carro",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
